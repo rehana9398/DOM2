c46972b594d3bcfdb98283f3760bc0ce116b1abd
rehana9398 <rehanashaik4099@gmail.com>
2025-02-25 17:50:11 +0530

    Add files via upload

---
+++ DOM2.js
@@ -0,0 +1,17 @@
+
+var con = document.querySelector("#container");
+var love = document.querySelector("i");
+
+con.addEventListener("dblclick",function(){
+    love.style.transform =1.0;
+
+    love.style.transform ="translate(-50%,-50%) scale(1)";
+    setTimeout(function (){
+        love.style.transform ="translate(-50%,-50%) scale(0)";
+
+        
+    },2000)
+    // setTimeout(function (){
+    //     love.style.opacity = 1;
+    // },1000)
+});
